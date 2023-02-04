78908b9aa1da0a0e439c6771635974cfd022d1f6
DannySun17 <56458674+DannySun17@users.noreply.github.com>
2023-02-03 21:06:50 -0500

    Add files via upload

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject enemy;
+    public int postion_x;
+    public int postion_z;
+    public int enemyCount;
+
+private void Start() 
+{
+StartCoroutine(EnemySpawn());    
+}
+
+
+   IEnumerator EnemySpawn()
+   {
+    while(enemyCount < 10)
+    {
+        postion_x = Random.Range(1,50);
+        postion_z = Random.Range(1,30);
+        Instantiate(enemy, new Vector3(postion_x, 2, postion_z), Quaternion.identity);
+        yield return new WaitForSeconds(0.5f);
+        enemyCount +=1;
+
+    }
+   }
+}
