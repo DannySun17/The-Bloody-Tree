1ac195b0d0e05889965298c721e8313b61c7d2b7
DannySun17 <56458674+DannySun17@users.noreply.github.com>
2023-02-04 23:43:47 -0500

    Update Energy.cs

--- Assets/Scripts/Energy.cs
+++ Assets/Scripts/Energy.cs
@@ -1,31 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using TMPro;
-
 public class Energy : MonoBehaviour
 {
+    public float speed;
+
+    public Transform target;
+
+    public float chaseRadius;
+
     public static int energy = 0;
-    public TextMeshPro energyText;
+
+    public TextMeshProUGUI energyText;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        CheckDistance();
     }
+
+    void CheckDistance()
+    {
+        if (Vector3.Distance(target.position, transform.position) <= chaseRadius)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+    }
+
+
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.tag == "Player")
+        if (other.gameObject.tag == "Principal Root")
         {
-            energy = + 1;
-            Destroy(other.gameObject);
+            energy = +1;
+
+            energyText.text = "Energy: " + energy.ToString();
+
+            DestroyImmediate(this.gameObject);
 
         }
     }
